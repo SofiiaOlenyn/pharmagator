c20ed2f080767ecba8f2ee5597dffb55b5ff6736
SofiyaOlenyn <olenynsofiya@gmail.com>
2021-10-07 16:50:17 +0300

    add base controller and service

---
+++ api/src/main/java/com/eleks/academy/pharmagator/service/HelloService.java
@@ -0,0 +1,9 @@
+package com.eleks.academy.pharmagator.service;
+
+import org.springframework.stereotype.Service;
+@Service
+public class HelloService {
+    public String helloWord() {
+        return "Hello word!";
+    }
+}
