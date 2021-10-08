5bc932cbfb7f0c5c11c33b19b7f7a0706e17075e
SofiiaOlenyn <sofiia.olenyn@ukma.edu.ua>
2021-10-08 19:33:44 +0300

    complete PharmacyController

--- api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
+++ api/src/main/java/com/eleks/academy/pharmagator/controllers/PharmacyController.java
@@ -4,20 +4,44 @@ import com.eleks.academy.pharmagator.entities.Pharmacy;
 import com.eleks.academy.pharmagator.repositories.PharmacyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/pharmacies")
 public class PharmacyController {
     private final PharmacyRepository pharmacyRepository;
 
+
     @GetMapping
     public ResponseEntity<List<Pharmacy>> getAll() {
         return ResponseEntity.ok(pharmacyRepository.findAll());
     }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Optional<Pharmacy>> getById(@PathVariable("id") Long id) {
+        return ResponseEntity.ok(pharmacyRepository.findById(id));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/addPharmacy")
+    public ResponseEntity<Pharmacy> create(@RequestBody Pharmacy pharmacy) {
+        return ResponseEntity.ok(pharmacyRepository.save(pharmacy));
+    }
+
+    @PostMapping("/update/{id}")
+    public ResponseEntity<Pharmacy> update(@PathVariable("id") long id, @RequestBody Pharmacy pharmacy) {
+        pharmacy.setId(id);
+        return ResponseEntity.ok(pharmacyRepository.save(pharmacy));
+
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public ResponseEntity deleteById(@PathVariable("id") Long id) {
+        pharmacyRepository.deleteById(id);
+        return ResponseEntity.ok(id + " is deleted");
+    }
+
 }
