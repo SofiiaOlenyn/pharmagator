b6e624d64c11c24645f4f257a27cc7efa5cc26bb
SofiiaOlenyn <olenynsofiya@gmail.com>
2021-10-08 14:10:33 +0300

    add code from lecture #4

--- api/src/main/java/com/eleks/academy/pharmagator/service/HelloService.java
+++
@@ -1,9 +0,0 @@
-package com.eleks.academy.pharmagator.service;
-
-import org.springframework.stereotype.Service;
-@Service
-public class HelloService {
-    public String helloWord() {
-        return "Hello word!!!";
-    }
-}
